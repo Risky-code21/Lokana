e41e02a70f563f99e5d921ff7b1a5eed70ff096f
Risky-code <iputuriskyandikaputra@gmail.com>
2026-01-18 20:54:48 +0800

    chore: setup migration, model, serta file lainnya untuk insiasi database awal

---
+++ app/Http/Controllers/UmkmProfileController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUmkmProfileRequest;
+use App\Http\Requests\UpdateUmkmProfileRequest;
+use App\Models\UmkmProfile;
+
+class UmkmProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUmkmProfileRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UmkmProfile $umkmProfile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UmkmProfile $umkmProfile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUmkmProfileRequest $request, UmkmProfile $umkmProfile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UmkmProfile $umkmProfile)
+    {
+        //
+    }
+}
---
+++ database/seeders/PelakuUmkmSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PelakuUmkmSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
