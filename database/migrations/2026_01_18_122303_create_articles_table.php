e728f61813d18b1cee43ea9b24848ae1eaccea58
Risky-code <iputuriskyandikaputra@gmail.com>
2026-01-19 21:06:14 +0800

    fix: penghapusan column author pada article migration

--- database/migrations/2026_01_18_122303_create_articles_table.php
+++ database/migrations/2026_01_18_122303_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
+            // $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
             $table->string('title');
             $table->string('slug')->unique();
             $table->string('short_text');
