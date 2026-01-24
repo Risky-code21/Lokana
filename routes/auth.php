19001d55b1086944853186f7012c88d848898400
Risky-code <iputuriskyandikaputra@gmail.com>
2026-01-24 14:43:52 +0800

    feat: menambahkan route khusus untuk login dan registrasi

---
+++ routes/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function () {
+    // Register route
+    Route::get('register', RegisterController::class, 'index')->name('register.index');
+    Route::post('/register', RegisterController::class, 'store')->name('register.store');
+
+    // Login route
+    Route::get('login', AuthController::class, 'index')->name('login.index');
+    Route::post('/login', AuthController::class, 'store')->name('login.store');
+});
