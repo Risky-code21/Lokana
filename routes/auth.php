6752d21a5e93990570fb51f495983b4f9a67a7e0
Risky-code <iputuriskyandikaputra@gmail.com>
2026-01-26 12:44:11 +0800

    fix: memperbaiki square bracket untuk pemanggilan controller didalam auth route

--- routes/auth.php
+++ routes/auth.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('')->group(function () {
     // Register route
-    Route::get('register', RegisterController::class, 'index')->name('register.index');
-    Route::post('/register', RegisterController::class, 'store')->name('register.store');
+    Route::get('register', [RegisterController::class, 'index'])->name('register.index');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
     // Login route
-    Route::get('login', AuthController::class, 'index')->name('login.index');
-    Route::post('/login', AuthController::class, 'store')->name('login.store');
+    Route::get('login', [AuthController::class, 'index'])->name('login.index');
+    Route::post('/login', [AuthController::class, 'store'])->name('login.store');
 });
