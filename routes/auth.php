1edc5be5bf3866a59c3206e91cf30dba33584f09
Risky-code <iputuriskyandikaputra@gmail.com>
2026-01-26 12:29:31 +0800

    fix: memperbaiki pengambilan path pada controller yang digunakan pada auth route

--- routes/auth.php
+++ routes/auth.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('')->group(function () {
+Route::middleware('guest')->group(function () {
     // Register route
     Route::get('register', RegisterController::class, 'index')->name('register.index');
     Route::post('/register', RegisterController::class, 'store')->name('register.store');
